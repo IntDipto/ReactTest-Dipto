8eaf26b46d16671269126b7ab2018138085d752b
IntDipto <dipto.dey@intelli.global>
2022-10-30 15:48:03 +0600

    product showing page completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { createBrowserHistory } from '@remix-run/router';
-import Products from './Layout/Products/Products';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Products from './Products/Products';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
+      loader: () => fetch('products.json'),
+      element : <Products></Products>
+    },
+    {
+      path: '/product-details',
       loader: () => fetch('products.json'),
       element : <Products></Products>
     }
---
+++ src/Product-details/productDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function productDetails() {
+  return (
+    <div className='container'>
+        <div className="img-slider">
+
+        </div>
+        <div className="details">
+
+        </div>
+    </div>
+  )
+}
+
+export default productDetails
