140752bbb8dd9e3109cc39806df405f936e11d36
IntDipto <dipto.dey@intelli.global>
2022-10-30 12:08:37 +0600

    react init and routeing setup completed

---
+++ src/Layout/Products/Products.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function Products() {
+    const [products, setProducts] = useState()
+  return (
+    <div className='products'>
+        <h3>Recommend only for you</h3>
+    </div>
+  )
+}
+
+export default Products
